9a8d4ced17702c0dbb99fad7bb192202bfcd796a
Antonio Rudas Sanchez <62669845+skyter-ajr@users.noreply.github.com>
2021-02-02 10:40:17 -0500

    Update ejercicio cibercafe.cpp

--- ejercicio cibercafe.cpp
+++ ejercicio cibercafe.cpp
@@ -5,22 +5,30 @@ using namespace std;
 
 struct equipos{
 	int numero_equipo;
-	string nombre_usuario;
+	string nombre_cliente;
 	int tiempo_en_horas;
 }e[100];
 
 int main(){
 	
-	int fin = 0;
+	int fin = 1;
 	int i=0;
 	
-	while(fin != 1){
+	while(fin != 0){
 		cout << "Que equipo desea usar?: ";
-		cin >> e.numero_equipo[i];
+		cin >> e[i].numero_equipo;
+		cin.ignore();
 		cout << "Nombre: ";
-		cin >> e.nombre_usuario[i];
+		getline(cin, e[i].nombre_cliente);
 		cout << "Tiempo de uso en horas: ";
+		cin >> e[i].tiempo_en_horas;
 		
+		cout << "\nnumero de equipo: " << e[i].numero_equipo << endl;
+		cout << "nombre del cliente: " << e[i].nombre_cliente << endl;
+		cout << "horas de uso: " << e[i].tiempo_en_horas << endl;
+		
+		cout << "Desea terminar la funcion?\0- si\n1- no : ";
+		cin >> fin;
 	}
 	
 	getch();
