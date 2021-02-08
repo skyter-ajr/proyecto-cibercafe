d3dac76511d87762e89717bf5146daa53570c3df
Antonio Rudas Sanchez <62669845+skyter-ajr@users.noreply.github.com>
2021-02-08 16:02:49 -0500

    Update ejercicio cibercafe.cpp

--- ejercicio cibercafe.cpp
+++ ejercicio cibercafe.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
-struct equipos{
+int fin = 1;
+int i=0, equipo[100];
+int total;
+
+struct Equipos{
 	int numero_equipo;
 	string nombre_cliente;
 	int tiempo_en_horas;
@@ -11,25 +15,41 @@ struct equipos{
 
 int main(){
 	
-	int fin = 1;
-	int i=0;
+	cout << "Presione ENTER para empezar.";
 	
 	while(fin != 0){
-		cout << "Que equipo desea usar?: ";
-		cin >> e[i].numero_equipo;
+		
 		cin.ignore();
 		cout << "Nombre: ";
 		getline(cin, e[i].nombre_cliente);
+		
 		cout << "Tiempo de uso en horas: ";
 		cin >> e[i].tiempo_en_horas;
+		cin.ignore();
+		cout << "Que equipo desea usar?: ";
+		cin >> e[i].numero_equipo;
 		
-		cout << "\nnumero de equipo: " << e[i].numero_equipo << endl;
-		cout << "nombre del cliente: " << e[i].nombre_cliente << endl;
-		cout << "horas de uso: " << e[i].tiempo_en_horas << endl;
+		while(equipo[e[i].numero_equipo] == 1){
+			cout << "equipo ocupado, elija uno diferente: ";
+			cin >> e[i].numero_equipo;
+		}
+		cin.ignore();
 		
-		cout << "Desea terminar la funcion?\0- si\n1- no : ";
+		if(equipo[e[i].numero_equipo] == 0){
+			equipo[e[i].numero_equipo] = 1;
+			cout << "\nSu equipo esta listo para su uso." << endl;
+		}
+		
+		total = e[i].tiempo_en_horas * 10000;
+		cout << "El total a pagar es de " << total << endl;
+		
+		i++;
+		
+		cout << "\nDesea terminar la funcion?: \n0- si\n1- no : ";
 		cin >> fin;
 	}
+	
+	cout << "Gracias por usar nuestros servicio." << endl;
 	
 	getch();
 	return 0;
